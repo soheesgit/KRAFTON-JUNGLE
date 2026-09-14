1a08d21651759399f8825c8303642c1096701946
soheesgit <144293475+soheesgit@users.noreply.github.com>
2026-09-14 14:21:10 +0900

    링크드리스트 3번 문제 풀이

--- week4_5/Data-Structures/Linked_List/Q3_A_LL.c
+++ week4_5/Data-Structures/Linked_List/Q3_A_LL.c
@@ -86,7 +86,95 @@ int main()
 
 void moveOddItemsToBack(LinkedList *ll)
 {
-	/* add your code here */
+	// 짝수 링크드리스트와 홀수 링크드리스트 생성
+	LinkedList *odd = malloc(sizeof(LinkedList));
+	LinkedList *even = malloc(sizeof(LinkedList));
+
+	// 짝수 노드와 홀수 노드, 현재 탐색중인 노드 생성 
+	ListNode *oddNode, *evenNode, *nowNode;
+
+	oddNode = malloc(sizeof(ListNode));
+	evenNode = malloc(sizeof(ListNode));
+
+	odd->head = oddNode;
+	even->head = evenNode;
+
+	nowNode = ll->head;
+	
+	// 현재 노드를 하나씩 순회하면서 탐색
+	while (nowNode != NULL) {
+		int num = nowNode->item;
+		
+		if (num % 2 == 0) { // 짝수
+			// 다음 노드를 만들어서 이어두고, 값을 할당함
+			evenNode->next = malloc(sizeof(ListNode)); // 이러면 메모리 누수가 발생하는 현상은 어떻게? 
+			evenNode = evenNode->next;
+			evenNode->item = nowNode->item; 
+			evenNode->next = NULL; // 다음 값을 비워둠 (마지막 노드에 쓰레기 노드 출력 방지)
+		} else {
+			// 홀수
+			oddNode->next = malloc(sizeof(ListNode));
+			oddNode = oddNode->next;
+			oddNode->item = nowNode->item;
+			oddNode->next = NULL;
+		}
+
+		nowNode = nowNode->next;
+	}
+
+	// 짝수 노드의 맨 마지막에 홀수 노드의 시작을 붙임. 처음엔 쓰레기값이 있을 수도 있으니 next후 붙여줌
+	evenNode->next = odd->head->next;
+	
+	ll->head = even->head->next;
+	return;
+}
+
+void bestOddItemsToBack(LinkedList *ll) {
+	ListNode *evenHead = NULL;
+	ListNode *oddHead = NULL;
+	ListNode *evenTail = NULL;
+	ListNode *oddTail = NULL;
+
+	ListNode *cur = ll->head;
+
+	while (cur != NULL) {
+		ListNode *next = cur->next; // 아직 next값을 가지고 있는 노드 연결
+
+		cur->next = NULL; // 기존 연결을 끊어둠
+
+		if (cur->item % 2 == 0) { // 짝수의 경우
+			if (evenHead == NULL) {
+				evenHead = cur;
+				evenTail = cur;
+			} else {
+				evenTail->next = cur; //짝수 다음 노드를 cur 으로 지정한다. next값이 null이니 노드 하나만 연결된다.
+				evenTail = cur;
+			}
+		}
+		else { // 홀수의 경우
+			if (oddHead = NULL) {
+				oddHead = cur;
+				oddTail = cur;
+			} else {
+				oddTail->next = cur;
+				oddTail = cur; 
+			}
+		}
+
+		cur = next;
+	}
+
+	if (evenHead == NULL)
+    {
+        // 전부 홀수
+        ll->head = oddHead;
+    }
+    else
+    {
+        // 짝수 뒤에 홀수 연결
+        evenTail->next = oddHead;
+        ll->head = evenHead;
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
